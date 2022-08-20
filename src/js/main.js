368ec27b41d5c1ef49f0b30756f578655279ec40
Victor Larodiel <victor.larodiel@hotmail.com>
2022-08-19 21:38:44 -0300

    Victor's Larodiel test

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,31 @@
-var reportsWidget = {
+const reportsWidget = {
     options: {
-        containerSelector: '.reports',
-        template: (
-            '{{#.}}' +
-                '<article class="reports_item">' +
-                    '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
-                    '</a>' +
-                    '<footer class="reports_docs">' +
-                        '{{#documents}}' +
-                            '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
-                            '</h3>' +
-                        '{{/documents}}' +
-                    '</footer>' +
-                '</article>' +
-            '{{/.}}'
-        )
+    containerSelector: '.reports',
+    template:
+        '{{#.}}' +
+            '<article class="reports_item">' +
+                '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover" role="presentation" width="240" height="300"/>' +
+                '<footer class="reports_docs">' +
+                    '{{#documents}}' +
+                        '<h2 class="reports_title">' +
+                            '<a class="reports_title_cta" href="{{url}}" target="_blank" title="{{title}}" download="{{title}}" target="_blank">' +
+                                '{{ title }} <span class="reports_file_details">({{ file_size }} {{ file_type }})</span>' +
+                            '</a>' +
+                        '</h2>' +
+                    '{{/documents}}' +
+                '</footer>' +
+            '</article>' +
+        '{{/.}}',
     },
-
-    init: function() {
-        this.renderReports(reportData || []);
+    renderReports: function (reports) {
+    const that = this;
+    const options = that.options;
+    document.querySelector(options.containerSelector).innerHTML =
+        Mustache.render(options.template, reports);
     },
-
-    renderReports: function(reports) {
-        var inst = this,
-            options = inst.options;
-
-        $(options.containerSelector).html(Mustache.render(options.template, reports));
-    }
+    init: function () {
+    this.renderReports(reportData || []);
+    },
 };
 
 reportsWidget.init();
